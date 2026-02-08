f431abdb2127f7a46ee8fd9a7993cc54232b3c77
Orka <orkabits@gmail.com>
2026-02-07 22:29:57 -0500

    feat: add custom marker icon functionality to leaflet
    - Introduced a new marker icon with a cyan SVG design and a pulse effect for enhanced visual feedback on the map.
    - Added CSS styles for the marker icon and its selected state, improving user interaction and aesthetics.
    - Updated JavaScript to create and bind the new marker icon, ensuring it integrates seamlessly with existing leaflet functionality.

---
+++ static/js/plugins/leaflet.markerIcon.js
@@ -0,0 +1,65 @@
+"use strict"
+
+import "../leaflet.js"
+;(function (factory) {
+	var L
+	if (typeof define === "function" && define.amd) {
+		define(["leaflet"], factory)
+	} else if (typeof module !== "undefined") {
+		L = require("leaflet")
+		module.exports = factory(L)
+	} else {
+		if (typeof window.L === "undefined") {
+			throw new Error("Leaflet must be loaded first")
+		}
+		factory(window.L)
+	}
+})(function (L) {
+	L.MarkerIcon = {
+		_svg:
+			'<svg xmlns="http://www.w3.org/2000/svg" width="20" height="28" viewBox="0 0 20 28">' +
+			'<path d="M10 0C4.477 0 0 4.477 0 10c0 7 10 18 10 18s10-11 10-18C20 4.477 15.523 0 10 0z" fill="#00e5ff"/>' +
+			'<circle cx="10" cy="10" r="4" fill="#0a1628"/>' +
+			"</svg>",
+
+		createPair: function (extraClass) {
+			var cls = "marker-location" + (extraClass ? " " + extraClass : "")
+			var html =
+				'<div class="marker-location-icon">' +
+				this._svg +
+				"</div>" +
+				'<div class="marker-location-pulse"></div>'
+
+			var icon = L.divIcon({
+				className: cls,
+				html: html,
+				iconSize: [20, 28],
+				iconAnchor: [10, 28],
+				popupAnchor: [0, -30],
+				tooltipAnchor: [12, -20]
+			})
+
+			var greyscaleIcon = L.divIcon({
+				className: cls + " marker-location-greyscale",
+				html: html,
+				iconSize: [20, 28],
+				iconAnchor: [10, 28],
+				popupAnchor: [0, -30],
+				tooltipAnchor: [12, -20]
+			})
+
+			return { icon: icon, greyscaleIcon: greyscaleIcon }
+		},
+
+		bindSelection: function (marker) {
+			marker.on("popupopen", function () {
+				var el = marker.getElement()
+				if (el) L.DomUtil.addClass(el, "marker-selected")
+			})
+			marker.on("popupclose", function () {
+				var el = marker.getElement()
+				if (el) L.DomUtil.removeClass(el, "marker-selected")
+			})
+		}
+	}
+})
