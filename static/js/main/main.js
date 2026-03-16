7a8be5360f2dadb503d770a726758bd0117f4cdd
Orka <orkabyte@proton.me>
2026-03-16 15:53:07 -0400

    feat: add default layer property for grid and update layer initialization logic
    - Introduced a default property for the grid layer in main.js to streamline layer management.
    - Updated the layer initialization condition in leaflet.urllayers.js to check for the default property, enhancing layer loading behavior.

--- static/js/main/main.js
+++ static/js/main/main.js
@@ -135,6 +135,7 @@ void (function (global) {
 			{
 				name: "grid",
 				layer: grid,
+				default: true,
 				icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><line x1="6" y1="1" x2="6" y2="17"/><line x1="12" y1="1" x2="12" y2="17"/><line x1="1" y1="6" x2="17" y2="6"/><line x1="1" y1="12" x2="17" y2="12"/></svg>'
 			},
 			{
